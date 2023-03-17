d5891a920dfc07f14e240ec2d7f2095859011e7d
mabrady444 <bradymadel@gmail.com>
2023-03-17 15:58:42 -0400

    fix the sh file

--- autorun/run-all.sh
+++ autorun/run-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd && cd Desktop/tele-ultrasound-2022-main/autorun && ./start-servers.sh & sleep 5 &&
-cd && cd Desktop/tele-ultrasound-2022-main/autorun && python replace-address.py
+cd && cd Desktop/tele-ultrasound-2022/autorun && ./start-servers.sh & sleep 5 &&
+cd && cd Desktop/tele-ultrasound-2022/autorun && python replace-address.py
--- autorun/start-servers.sh
+++ autorun/start-servers.sh
@@ -1,9 +1,9 @@
-cd && cd Desktop/tele-ultrasound-2022-main/controller && node xyController &
-cd && cd Desktop/tele-ultrasound-2022-main/controller && node tendonController &
-cd && cd Desktop/tele-ultrasound-2022-main/controller && node kzController &
+cd && cd Desktop/tele-ultrasound-2022/controller && node xyController &
+cd && cd Desktop/tele-ultrasound-2022/controller && node tendonController &
+cd && cd Desktop/tele-ultrasound-2022/controller && node kzController &
 sleep 5 &&
-cd && cd Desktop/tele-ultrasound-2022-main/controller && node poseController &
+cd && cd Desktop/tele-ultrasound-2022/controller && node poseController &
 sleep 5 &&
-cd && cd Desktop/tele-ultrasound-2022-main/server && node ws-server.js &
+cd && cd Desktop/tele-ultrasound-2022/server && node ws-server.js &
 sleep 5 &&
-cd && lt --host http://loca.lt --port 8080 2>&1 | tee Desktop/tele-ultrasound-2022-main/autorun/address.txt
+cd && lt --host http://loca.lt --port 8080 2>&1 | tee Desktop/tele-ultrasound-2022/autorun/address.txt
