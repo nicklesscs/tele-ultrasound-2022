f9653f067a1e356eed435a50824632daaef6ef9f
mabrady444 <bradymadel@gmail.com>
2023-03-17 14:57:00 -0400

    autorun nick

--- autorun/start-servers.sh
+++ autorun/start-servers.sh
@@ -1,9 +1,9 @@
-cd && cd Desktop/control/controller && node xyController &
-cd && cd Desktop/control/controller && node tendonController &
-cd && cd Desktop/control/controller && node kzController &
+cd && cd Desktop/tele-ultrasound-2022-main/controller && node xyController &
+cd && cd Desktop/tele-ultrasound-2022-main/controller && node tendonController &
+cd && cd Desktop/tele-ultrasound-2022-main/controller && node kzController &
 sleep 5 &&
-cd && cd Desktop/control/controller && node poseController &
+cd && cd Desktop/tele-ultrasound-2022-main/controller && node poseController &
 sleep 5 &&
-cd && cd Desktop/control/server && node ws-server.js &
+cd && cd Desktop/tele-ultrasound-2022-main/server && node ws-server.js &
 sleep 5 &&
-cd && lt --host http://loca.lt --port 8080 2>&1 | tee Desktop/control/autorun/address.txt
+cd && lt --host http://loca.lt --port 8080 2>&1 | tee Desktop/tele-ultrasound-2022-main/autorun/address.txt
