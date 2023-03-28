c05ce13c61f1703a7acf5e20326638f44cabaec4
nicklesscs <tgao2@wpi.edu>
2023-03-28 16:51:39 -0400

    receive tf_polhemus successfully

--- server/client.js
+++ server/client.js
@@ -1,55 +1,33 @@
 const WebSocket = require('ws');
 const ipc = require('node-ipc');
+
 const ws = new WebSocket('ws://130.215.175.244:8080');
 
 ws.on('open', function open() {
   console.log('connected to server');
-  ws.send("client connected from afar");
 });
 
-ws.onmessage = (event) => {
-  console.log(event.data);
-};
-
-// ws.on('message', function incoming(message) {
-//   // Parse incoming message as JSON
-//   let data = JSON.parse(message);
-
-//   // Check if pose controller is connected before sending data to it
-//   if (poseControlConnection) {
-//       // Send data to pose controller using IPC
-//       ipc.of.poseControl.emit('bodyData', data);
-//   }
-// });
+ws.on('message', function incoming(message) {
+  const data = JSON.parse(message);
+  if (data.type === 'tf_polhemus') {
+    console.log('Received tf_polhemus data:', data);
+  }
+});
 
 ws.on('close', function close() {
   console.log('disconnected from server');
 });
 
-// SETUP FOR IPC
-ipc.config.id = 'ws-server';
+ipc.config.id = 'client';
 ipc.config.retry = 1500;
 ipc.config.silent = true;
-//let tendonControlConnection = false;
-//let xyControlConnection = false;
-//let kzControlConnection = false;
-let poseControlConnection = false;
 
-ipc.connectTo('poseControl', function () {
-	poseControlConnection = true;
-	ipc.of.poseControl.on('connect', function () {
-		ipc.log('## connected to poseControl ##'.rainbow, ipc.config.delay);
-		ipc.of.poseControl.emit('message', 'hello');
-		console.log('ws-server connected to poseControl');
-	});
-	ipc.of.poseControl.on('disconnect', function () {
-		ipc.log('disconnected from world'.notice);
-		console.log('ws-server disconnected from kzControl');
-		poseControlConnection = false;
-	});
+ipc.connectTo('server', function () {
+  ipc.of.server.on('connect', function () {
+    ipc.log('## connected to server ##'.rainbow, ipc.config.delay);
+    ipc.of.server.emit('message', 'hello');
+  });
+  ipc.of.server.on('disconnect', function () {
+    ipc.log('disconnected from server'.notice);
+  });
 });
-
-
-
-
-
