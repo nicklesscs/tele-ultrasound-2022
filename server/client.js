7b0262875ed5158fcdf718270950708565ebfbf9
nicklesscs <tgao2@wpi.edu>
2023-03-29 13:54:37 -0400

    fix the data issue

--- server/client.js
+++ server/client.js
@@ -4,18 +4,30 @@ const ipc = require('node-ipc');
 const ws = new WebSocket('ws://130.215.175.244:8080');
 
 ws.on('open', function open() {
-  console.log('connected to server');
+  console.log('Connected to server');
 });
 
 ws.on('message', function incoming(message) {
-  const data = JSON.parse(message);
-  if (data.type === 'tf_polhemus') {
-    console.log('Received tf_polhemus data:', data);
+  try {
+    const data = JSON.parse(message);
+    if (data.type === 'tf_polhemus') {
+      console.log('Received tf_polhemus data:', data);
+    }
+  } catch (error) {
+    console.error(`Error parsing message: ${error}`);
   }
 });
 
 ws.on('close', function close() {
-  console.log('disconnected from server');
+  console.log('Disconnected from server');
+  setTimeout(() => {
+    console.log('Reconnecting to server...');
+    ws.connect();
+  }, 2000);
+});
+
+ws.on('error', function error(error) {
+  console.error(`WebSocket error: ${error}`);
 });
 
 ipc.config.id = 'client';
@@ -24,10 +36,10 @@ ipc.config.silent = true;
 
 ipc.connectTo('server', function () {
   ipc.of.server.on('connect', function () {
-    ipc.log('## connected to server ##'.rainbow, ipc.config.delay);
+    ipc.log('## Connected to server ##'.rainbow, ipc.config.delay);
     ipc.of.server.emit('message', 'hello');
   });
   ipc.of.server.on('disconnect', function () {
-    ipc.log('disconnected from server'.notice);
+    ipc.log('Disconnected from server'.notice);
   });
 });
